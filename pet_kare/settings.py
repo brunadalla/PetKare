c54b9c620fffe41292ca7cdf9fcfbc8402b4a05c
Bruna <bruna.dall9741@gmail.com>
2022-11-29 18:43:33 -0300

    Tarefa 2 - Serializers básicos

--- pet_kare/settings.py
+++ pet_kare/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "groups",
     "pets",
     "traits",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
---
+++ traits/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class TraitSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    create_at = serializers.DateTimeField(read_only=True)
